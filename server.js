754ed742485ef4b292dd39b94d896013ed781a00
vdemchuk <vasyl.demchuk@gmail.com>
2015-05-21 19:48:55 +0300

    added Prod

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var http = require("http");
 
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello From PreProd");
+  response.write("Hello From Prod");
   response.end();
-}).listen(8001);
+}).listen(8002);
