496d4f306e5b13d5118db731b9c959223718a057
vdemchuk <vasyl.demchuk@gmail.com>
2015-05-21 19:29:06 +0300

    preProd branch

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var http = require("http");
 
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello From Beta");
+  response.write("Hello From PreProd");
   response.end();
-}).listen(8000);
+}).listen(8001);
